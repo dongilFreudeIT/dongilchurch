57f7c6e5b1aa7204d959232801b2c83acc59e557
guntiger <36735411+guntiger@users.noreply.github.com>
2019-05-16 16:04:46 +0900

    알람 10개씩 추가 로드, 삭제 수정

--- src/pages/alarm/alarm.ts
+++ src/pages/alarm/alarm.ts
@@ -12,7 +12,6 @@ import { Storage } from "@ionic/storage";
 import { HTTP } from "@ionic-native/http";
 
 import { PushShowPage } from "../pushShow/pushShow";
-import { AlarmHidePage } from "../alarm-hide/alarm-hide";
 import * as moment from "moment";
 import { NativeStorage } from "@ionic-native/native-storage";
 
@@ -23,8 +22,9 @@ import { NativeStorage } from "@ionic-native/native-storage";
 export class AlarmPage {
   url: string = "http://13.125.35.123/api";
   pushDataArray: any; //서버에서 받아온 푸쉬 메세지 리스트 저장 array
-  bInfinite: boolean = true;
   bShowAlert: boolean = false;
+  nPushList: number;
+  nCurPush: number = 10;
 
   constructor(
     public loadingCtrl: LoadingController,
@@ -43,6 +43,8 @@ export class AlarmPage {
 
     moment.lang("ko");
 
+    this.getNumberOfPush();
+
     document.addEventListener("resume", () => {
       console.log("document.addEventListener('resume' ");
       this._zone.run(() => {
@@ -50,22 +52,19 @@ export class AlarmPage {
       });
     });
   }
-  doInfinite(): Promise<any> {
+
+  doInfinite(infiniteScroll) {
     console.log("Begin async operation");
     this.getPushMessageAll();
-    return new Promise(resolve => {
-      setTimeout(() => {
-        console.log("Async operation has ended");
-        resolve();
-        this.bInfinite = false;
-      }, 10000);
-    });
+    setTimeout(() => {
+      console.log("Async operation has ended");
+      infiniteScroll.complete();
+    }, 500);
   }
 
   doRefresh(refresher) {
     console.log("Begin async operation", refresher);
     this.getPushMessage();
-    this.bInfinite = true;
     setTimeout(() => {
       console.log("Async operation has ended");
       refresher.complete();
@@ -93,7 +92,7 @@ export class AlarmPage {
               .setItem("pushDataArray2", this.pushDataArray)
               .then(
                 () => {
-                  console.log(JSON.stringify(this.pushDataArray,null,2));
+                  // console.log(JSON.stringify(this.pushDataArray, null, 2));
                 },
                 error => console.error("Error storing item", error)
               );
@@ -108,7 +107,9 @@ export class AlarmPage {
   getPushMessageAll() {
     //저장 된 user serial 가져와서 서버에 푸쉬 리스트 요청
     this.storage.get("user_serial").then(value => {
-      var param = { serial: value };
+      this.nCurPush = this.nCurPush + 10;
+      console.log(this.nCurPush);
+      var param = { serial: value, nPushList: this.nCurPush };
       this.http
         .post(this.url + "/user/get_push_list_all", param, {})
         .then(data => {
@@ -126,6 +127,7 @@ export class AlarmPage {
                   () => console.log("Stored item!"),
                   error => console.error("Error storing item", error)
                 );
+              console.log("obj" + JSON.stringify(obj));
             } else {
               console.log(obj.message);
             }
@@ -176,8 +178,6 @@ export class AlarmPage {
           text: "취소",
           handler: data => {
             console.log("Cancel clicked");
-
-            this.bShowAlert = false;
           }
         },
         {
@@ -185,31 +185,39 @@ export class AlarmPage {
           handler: data1 => {
             console.log("Saved clicked");
             this.deletePush(data);
-            this.bShowAlert = false;
           }
         }
       ]
     });
+    alert.onDidDismiss(() => {
+      console.log("The alert has been closed.");
+      this.bShowAlert = false;
+    });
     alert.present();
   }
 
-  deletePush(data) {
-    this.presentLoading();
-    console.log("DELETE : " + data.serial);
+  deletePush(delData) {
+    // this.presentLoading();
+    console.log("DELETE : " + delData.serial);
+    this._zone.run(() => {
+      this.pushDataArray = this.pushDataArray.filter(obj => obj !== delData);
+      // console.log(JSON.stringify(this.pushDataArray))
+    });
+
     this.http
-      .post(this.url + "/user/hidePush/", { serial: data.serial }, {})
+      .post(this.url + "/user/hidePush/", { serial: delData.serial }, {})
       .then(data => {
         if (data.status == 200) {
           var obj = JSON.parse(data.data);
           console.log(data.data);
           if (obj.code.includes("S")) {
-            this._zone.run(() => {
-              // this.pushDataArray = this.pushDataArray.filter(item => item !== data);
-              this.getPushMessage();
-              // this.pushDataArray.remove(data);
-              console.log(JSON.stringify(data));
-            });
+            // this.pushDataArray = this.pushDataArray.filter(item => item !== data);
+            // this.getPushMessage();
+            // this.pushDataArray.splice(data,1);
+            // this.pushDataArray.remove(data);
+            // console.log(JSON.stringify(data));
             // this.getUsersInfo();
+            console.log("deletePush success");
           } else if (obj.code.includes("E")) {
           }
         }
@@ -232,6 +240,12 @@ export class AlarmPage {
   }
   ionViewDidLeave() {
     console.log("leave");
+    this.nativeStorage
+      .setItem("pushDataArray2", this.pushDataArray)
+      .then(
+        () => console.log("Stored item!"),
+        error => console.error("Error storing item", error)
+      );
   }
   ionViewDidLoad() {
     console.log("ionViewDidLoad");
@@ -244,5 +258,25 @@ export class AlarmPage {
   }
   public close(item: ItemSliding) {
     item.close();
+  }
+
+  getNumberOfPush() {
+    //저장 된 user serial 가져와서 서버에 푸쉬 리스트 요청
+    this.storage.get("user_serial").then(value => {
+      var param = { serial: value };
+      this.http.post(this.url + "/user/get_num_push", param, {}).then(data => {
+        if (data.status == 200) {
+          var obj = JSON.parse(data.data);
+          //로그인 성공이면
+          if (obj.code == "S01") {
+            this.nPushList = obj.value[0].number;
+            // console.log(JSON.stringify(obj.value[0].number))
+            // console.log(this.nPushList);
+          } else {
+            console.log(obj.message);
+          }
+        }
+      });
+    });
   }
 }
